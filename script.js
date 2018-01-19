d1eb9dc77ce91ded4072ce9cf2948ec22ebcd86f
Leon <g739233944@gmail.com>
2018-01-19 20:34:11 +1100

    19/1 updated

---
+++ script.js
@@ -0,0 +1,46 @@
+function play() {
+  flash()
+  var  timer = setInterval("flash()", 2000)
+}
+
+function stop() {
+  window.location.reload()
+}
+
+function flash() {
+  // reset
+  var elements=document.getElementsByClassName('block');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor="";
+  }
+   // alert(elements.length)
+   // 3 random number
+  var array=[];
+  var i=0;
+  while (i<3) {
+    var num= Math.floor(9*Math.random());
+    if (array.includes(num)) {
+      continue;
+    }
+    array[i]=num;
+    i++;
+  }
+  // alert(array)
+  // random color
+  var color=[];
+  color[3]="#fea500";
+  var i=0;
+  while (i<3) {
+    var num= Math.floor(0xFFFFFF*Math.random());
+    if (color.includes(num)) {
+      continue;
+    }
+    color[i]='#'+num.toString(16);
+    i++;
+  }
+  // alert(color)
+  // set background color
+  for (var i = 0; i < array.length; i++) {
+    elements[array[i]].style.backgroundColor=color[i];
+  }
+}
